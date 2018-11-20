5cda7da4bbdd0344220a83625342695c347e347e
sashinpivotal <sashin@pivotal.io>
2018-11-20 16:38:20 -0500

    Actuator Added

--- src/main/java/io.pivotal.pal.tracker/TimeEntryController.java
+++ src/main/java/io.pivotal.pal.tracker/TimeEntryController.java
@@ -1,6 +1,9 @@
 package io.pivotal.pal.tracker;
 
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,9 +15,13 @@ import java.util.List;
 public class TimeEntryController {
 
     private TimeEntryRepository timeEntryRepository;
+    private final DistributionSummary timeEntrySummary;
+    private final Counter actionCounter;
 
-    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+    public TimeEntryController(TimeEntryRepository timeEntryRepository, MeterRegistry meterRegistry) {
         this.timeEntryRepository = timeEntryRepository;
+        timeEntrySummary = meterRegistry.summary("timeEntry.summary");
+        actionCounter = meterRegistry.counter("timeEntry.actionCounter");
     }
 
     @PostMapping()
